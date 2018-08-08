17205967642ae4b1d6fb0fb5a257d5610a2cccdd
Ravel Araujo <ravelramyres@hotmail.com>
2018-08-08 23:13:23 +0100

    Testando primeira classe de dominio - Categoria

---
+++ src/main/java/com/ravel/cursomc/domain/Categoria.java
@@ -0,0 +1,75 @@
+package com.ravel.cursomc.domain;
+
+import java.io.Serializable;
+
+//classe serializable (Classe que implementa a serializable) para que o objeto possa ser gravado em arquivos
+//exigencia do Java
+public class Categoria implements Serializable {
+
+//	geração default
+	private static final long serialVersionUID = 1L;
+	
+	//atributos básicos
+	private Integer id;
+	private String nome;
+	
+	//Construtor vazio, cria o objeto sem jogar nada aos atributos
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	//metodos de acesso aos atributos (Gets e setters)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//hascode e equals **(Dois objetos para serem comparados pelos conteudos e nao ponteiro de memoria)**
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/com/ravel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ravel/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.ravel.cursomc.resources;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ravel.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		
+		//estanciando o objeto
+		Categoria cat = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }

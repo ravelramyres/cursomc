97114a161efc252c996dec09f7b7df25cd68aee1
Ravel Araujo <ravelramyres@hotmail.com>
2018-08-09 00:53:12 +0100

    Criando operação de instanciação

--- src/main/java/com/ravel/cursomc/CursomcApplication.java
+++ src/main/java/com/ravel/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.ravel.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.ravel.cursomc.domain.Categoria;
+import com.ravel.cursomc.repositories.CategoriaRepository;
 
+
+//CommandLineRunner = Faz a classe implementar a interface (implementa o metodo auxiliar para executar a ação quando iniciar)
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	//Instancia os objetos
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		//repositories salva os objetos
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
